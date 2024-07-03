7632b58ba51e3334ed377bd51b106c542042114e
Feruze21 <hebibasiferuze89@gmail.com>
2024-07-03 20:35:35 +0200

    added hello

--- src/gitHub/GitHub.java
+++ src/gitHub/GitHub.java
@@ -3,5 +3,7 @@ package gitHub;
 public class GitHub {
     public static void main(String[] args) {
         int mosha =20;
+        String emri= "Rezi";
+        System.out.println(" Hello");
     }
 }
