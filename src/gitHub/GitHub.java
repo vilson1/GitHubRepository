bc12fc7c57600c5061b353df95ac820d0a8c9d86
User <vilson.shehu@outlook.com>
2024-07-03 20:18:18 +0200

    newBranch

--- src/gitHub/GitHub.java
+++ src/gitHub/GitHub.java
@@ -1,7 +1,11 @@
 package gitHub;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GitHub {
     public static void main(String[] args) {
         int mosha =20;
+        List<String> emrat=new ArrayList<>();
     }
 }
